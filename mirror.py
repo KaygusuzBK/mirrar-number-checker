167c30b83597ac574f8b0cc5f68ec48d0f789407
Berkan Kaygusuz <74183528+KaygusuzBK@users.noreply.github.com>
2021-03-12 16:41:45 +0300

    Logic Kontrol Eklendi
    kullanıcıdan alınan sayının int , str olup olmadığı kontrol edildi

--- mirror.py
+++ mirror.py
@@ -1,20 +1,24 @@
 while True:
     isSameNumber = 0
     i = 0
-    enteredNumber = list(input('Enter a number: '))
-    reversedEnteredNumber = list(reversed(enteredNumber))
-    intenteredNumber = int(len(enteredNumber))
-    if enteredNumber == reversedEnteredNumber:
-        while(i < len(enteredNumber)):
-            if i != len(enteredNumber) and enteredNumber[0] == enteredNumber[i]:
-                i = i + 1
-                isSameNumber = 0
+    enteredNumber = input("Enter a number: ")
+    if enteredNumber.strip().isdigit() and False:
+        listEnteredNumber = list(str(enteredNumber))
+        reversedlistEnteredNumber = list(reversed(listEnteredNumber))
+        intlistEnteredNumber = int(len(listEnteredNumber))
+        if listEnteredNumber == reversedlistEnteredNumber:
+            while i < len(listEnteredNumber):
+                if i != len(listEnteredNumber) and listEnteredNumber[0] == listEnteredNumber[i]:
+                    i = i + 1
+                    isSameNumber = 0
+                else:
+                    isSameNumber = 1
+                    break
+            if isSameNumber == 1:
+                print("This is mirror number.")
             else:
-                isSameNumber = 1
-                break
-        if isSameNumber == 1:
-            print("this is mirror number.")
-        else:
-            print("this is not mirror number.")
-    elif enteredNumber != reversedEnteredNumber:
-        print("this is not mirror number")
+                print("This is not mirror number.")
+        elif listEnteredNumber != reversedlistEnteredNumber:
+            print("This is not mirror number")
+    else:
+            print('Please enter a valid number')
