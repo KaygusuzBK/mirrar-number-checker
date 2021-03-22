5e5232ae58743fc0af1665bd634a1be950bcd1e4
Berkan Kaygusuz <74183528+KaygusuzBK@users.noreply.github.com>
2021-03-22 21:53:24 +0300

    fonksiyon oluştturma
    program sadece fonksiyon içine yazıldı fakat hata var :-1:

---
+++ function.py
@@ -0,0 +1,3 @@
+def Mirror(enteredNumber):
+    Mirror =  
+    
--- mirror.py
+++ mirror.py
@@ -1,25 +1,28 @@
 import text
 while True:
-    isSameNumber = 0
-    i = 0
-    enteredNumber = input(text.ask)
-    if enteredNumber.strip().isdigit() and False:
-        listEnteredNumber = list(str(enteredNumber))
-        reversedlistEnteredNumber = list(reversed(listEnteredNumber))
-        intlistEnteredNumber = int(len(listEnteredNumber))
-        if listEnteredNumber == reversedlistEnteredNumber:
-            while i < len(listEnteredNumber):
-                if i != len(listEnteredNumber) and listEnteredNumber[0] == listEnteredNumber[i]:
-                    i = i + 1
-                    isSameNumber = 0
+    def Mirror():
+            
+        isSameNumber = 0
+        i = 0
+        enteredNumber = input(text.ask)
+        if enteredNumber.strip().isdigit() and False:
+            listEnteredNumber = list(str(enteredNumber))
+            reversedlistEnteredNumber = list(reversed(listEnteredNumber))
+            intlistEnteredNumber = int(len(listEnteredNumber))
+            if listEnteredNumber == reversedlistEnteredNumber:
+                while i < len(listEnteredNumber):
+                    if i != len(listEnteredNumber) and listEnteredNumber[0] == listEnteredNumber[i]:
+                        i = i + 1
+                        isSameNumber = 0
+                    else:
+                        isSameNumber = 1
+                        break
+                if isSameNumber == 1:
+                    print(text.succesfull)
                 else:
-                    isSameNumber = 1
-                    break
-            if isSameNumber == 1:
-                print(text.succesfull)
-            else:
+                    print(text.error)
+            elif listEnteredNumber != reversedlistEnteredNumber:
                 print(text.error)
-        elif listEnteredNumber != reversedlistEnteredNumber:
-            print(text.error)
-    else:
-            print(text.valid)
+        else:
+                print(text.valid)
+    Mirror()
